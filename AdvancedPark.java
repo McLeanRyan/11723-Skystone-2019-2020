2cc9e2775405c82e150f869cf3b635ec034ae38c
McLeanRyan <57815498+McLeanRyan@users.noreply.github.com>
2020-03-07 06:52:53 -0500

    Add files via upload

--- AdvancedPark.java
+++ AdvancedPark.java
@@ -1,10 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.ZYX;
 
 @Autonomous
 public class AdvancedPark extends LinearOpMode {
@@ -23,6 +33,19 @@ public class AdvancedPark extends LinearOpMode {
     boolean left = true;
 
     boolean a = false;
+    int Where =0;
+    BNO055IMU imu;
+    private Orientation angles;
+
+    double kP = 0.005;
+    double kD = 0.01;
+    double kI = 0.00008;
+
+    double totalError = 0;
+    double lastAngle = 0;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Mode", "Initialized");
@@ -65,6 +88,21 @@ public class AdvancedPark extends LinearOpMode {
         LB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BNO055IMU.Parameters parameters1 = new BNO055IMU.Parameters();       //sets up IMU
+        parameters1.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters1.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters1.calibrationDataFile = "REVHub1IMUCalibration.json";
+        parameters1.loggingEnabled      = true;
+        parameters1.loggingTag          = "IMU";
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters1);
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        telemetry.addLine("ready to go!");
+        telemetry.update();
+
 
         while (!a) {
             if (gamepad1.dpad_left) {
@@ -116,13 +154,13 @@ public class AdvancedPark extends LinearOpMode {
         }
         sleep(delay);
         if (far) {
-            encoderDrive(.25, 36, true);
+            encoderDrive(.25, 36, 31, true);
             sleep(500);
         }
-        encoderDrive(.25, distance, false);
+        encoderDrive(.25, distance, 31, false);
     }
 
-    private void encoderDrive(double speed, double inches, boolean strafe) {
+    private void encoderDrive(double speed, double inches, double timeoutS, boolean strafe) {
 
         telemetry.addLine("Encoder Drive");
 
@@ -134,6 +172,8 @@ public class AdvancedPark extends LinearOpMode {
         int rFPos = RF.getCurrentPosition();
         int lBPos = LB.getCurrentPosition();
         int rBPos = RB.getCurrentPosition();
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, ZYX, AngleUnit.DEGREES);
+        double startAngle = angles.firstAngle;
 
         if (opModeIsActive()) {
             if (strafe) {
@@ -154,8 +194,6 @@ public class AdvancedPark extends LinearOpMode {
             telemetry.addData("newRFTarget", newRFTarget);
             telemetry.addData("newLBTarget", newLBTarget);
             telemetry.addData("newRBTarget", newRBTarget);
-            telemetry.update();
-
 
             LF.setTargetPosition(newLFTarget);
             RF.setTargetPosition(newRFTarget);
@@ -172,22 +210,37 @@ public class AdvancedPark extends LinearOpMode {
             LB.setPower(Math.abs(speed));
             RB.setPower(Math.abs(speed));
 
-            while (opModeIsActive() && (LF.isBusy() && RF.isBusy() && LB.isBusy() && RB.isBusy())) {
-                telemetry.addData("newLFTarget", newLFTarget);
-                telemetry.addData("newRFTarget", newRFTarget);
-                telemetry.addData("newLBTarget", newLBTarget);
-                telemetry.addData("newRBTarget", newRBTarget);
-                telemetry.addData("LF Current Pos", LF.getCurrentPosition());
-                telemetry.addData("RF Current Pos", RF.getCurrentPosition());
-                telemetry.addData("LB Current Pos", LB.getCurrentPosition());
-                telemetry.addData("RB Current Pos", RB.getCurrentPosition());
+            runtime.reset();
+            while (opModeIsActive() && runtime.seconds() < timeoutS && (LF.isBusy() && RF.isBusy() && LB.isBusy() && RB.isBusy())) {
+                if (!strafe) {
+                    double error = kP * (startAngle - angles.firstAngle);
+                    angles = imu.getAngularOrientation(AxesReference.INTRINSIC, ZYX, AngleUnit.DEGREES);
+                    telemetry.addData("Start Angle", startAngle);
+                    telemetry.addData("Current Angle", angles.firstAngle);
+                    telemetry.addData("error", error);
+                    telemetry.update();
+                    if (error > 0) {
+                        LF.setPower(Math.abs(speed) - error);
+                        RF.setPower(Math.abs(speed) + error);
+                        LB.setPower(Math.abs(speed) - error);
+                        RB.setPower(Math.abs(speed) + error);
+                    } else if (error < 0) {
+                        LF.setPower(Math.abs(speed) + error);
+                        RF.setPower(Math.abs(speed) - error);
+                        LB.setPower(Math.abs(speed) + error);
+                        RB.setPower(Math.abs(speed) - error);
+                    }
+                }
+                telemetry.addData("LF Current Position", LF.getCurrentPosition());
+                telemetry.addData("RF Current Position", RF.getCurrentPosition());
+                telemetry.addData("LB Current Position", LB.getCurrentPosition());
+                telemetry.addData("RB Current Position", RB.getCurrentPosition());
                 telemetry.addData("LF Current Power", LF.getPower());
                 telemetry.addData("RF Current Power", RF.getPower());
                 telemetry.addData("LB Current Power", LB.getPower());
                 telemetry.addData("RB Current Power", RB.getPower());
                 telemetry.update();
             }
-
             LF.setPower(0);
             RF.setPower(0);
             LB.setPower(0);
@@ -198,7 +251,48 @@ public class AdvancedPark extends LinearOpMode {
             LB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             RB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            sleep(10000);
+            sleep(100);
+        }
+    }
+
+    private void gyroTurn(double targetAngle) {
+        //+ is counter-clockwise
+        //- is clockwise
+        boolean finished = false;
+        while (!finished) {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, ZYX, AngleUnit.DEGREES);
+            double currentAngle = angles.firstAngle;
+            double e = (targetAngle - currentAngle);
+            totalError += e;
+            double error = (kP * e) - (kD * (currentAngle - lastAngle)) + (kI) * (totalError);
+            lastAngle = currentAngle;
+            LF.setPower(-error);
+            RF.setPower(error);
+            LB.setPower(-error);
+            RB.setPower(error);
+            telemetry.addData("targetAngle", targetAngle);
+            telemetry.addData("currentAngle", currentAngle);
+            telemetry.addData("error", error);
+            telemetry.addData("targetAngle - currentAngle", targetAngle - currentAngle);
+            telemetry.addData("finished", finished);
+
+
+
+            telemetry.addData("LFM Current Power", LF.getPower());
+            telemetry.addData("RFM Current Power", RF.getPower());
+            telemetry.addData("LBM Current Power", LB.getPower());
+            telemetry.addData("RBM Current Power", RB.getPower());
+
+            telemetry.update();
+            if (Math.abs(targetAngle - currentAngle) < 4) {
+                finished = true;
+                telemetry.addData("Finished", finished);
+                RF.setPower(0);
+                RB.setPower(0);
+                LF.setPower(0);
+                LB.setPower(0);
+                sleep(500);
+            }
         }
     }
 }
